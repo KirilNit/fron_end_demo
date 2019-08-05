880ffa0bbcbd10d772704ca44b8459f1d3383145
KirillNitchenko <kirill.nitchenko@asferro.com>
2019-08-05 18:32:20 +0300

    add getNotes to the project

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {withStyles} from "@material-ui/core";
+import api from "../middleware/api"
 
 
 const styles = theme => (
@@ -15,13 +16,41 @@ class Notes extends Component{
     constructor(props) {
         super(props);
         this.state = {
-
+            notes:[]
         };
     };
+
+    componentDidMount(){
+        this.getNotes()
+    }
+
+    getNotes = () => {
+        api.get('http://127.0.0.1:5000/postnotes').getNote().then((resp)=>{this.setState({notes:resp})});
+        console.log('Current notes' + this.state.notes)
+
+    };
+
     render(){
         return(
             <div className={'rooted'}>
                 <p>This is Notes Page</p>
+                <div>{this.state.notes.map(
+                    (item, index) => {
+                        return(
+                            <div key={index}>
+                                {Object.keys(item).map(
+                                    (itemInner, index) => {
+                                        return (
+                                            <div key={index}>
+                                                <p>{itemInner}: {item[itemInner]}</p>
+                                            </div>
+                                        )
+                                    }
+                                )}
+                            </div>
+                        )
+                    }
+                )}</div>
             </div>
         )
     }
---
+++ src/middleware/api.js
@@ -0,0 +1,37 @@
+let api = {
+    get(url){
+        return {
+            getNote: () => fetch(url,{
+                method: 'GET'
+            }).then((res) => {
+                if(res.status === 401){
+                    return Promise.reject(new Error("Status 401"));
+                }
+                if(res.status === 403){
+
+                    return Promise.reject(new Error("Status 403"));
+                }
+                return res;
+            }).then((resp) => resp.json())
+        }
+    },
+    post(url, body){
+        return{
+            postNote: () => fetch(url,{
+                method: 'POST',
+                body: body
+            }).then((res) => {
+                if(res.status === 401){
+                    return Promise.reject(new Error("Status 401"));
+                }
+                if(res.status === 403){
+
+                    return Promise.reject(new Error("Status 403"));
+                }
+                return res;
+            }).then((resp) => resp.json())
+        }
+    }
+};
+
+export default api;
